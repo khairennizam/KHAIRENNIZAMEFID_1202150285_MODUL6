741e925b8862853606992ce69c79854fb7986c9b
khairennizam <khairennizam@gmail.com>
2018-04-01 21:33:39 +0700

    first commit

---
+++ app/src/main/java/com/example/khairen/khairennizamefid_1202150285_modul6/Model/User.java
@@ -0,0 +1,32 @@
+package com.example.khairen.khairennizamefid_1202150285_modul6.Model;
+
+
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
